333e869b611b2e9498fa088173ae08e6ba7be5d0
a19jorgepb <a19jorgepb@iessanclemente.net>
2020-04-18 15:20:39 +0200

    Mi primer commit

---
+++ Programa.java
@@ -0,0 +1,38 @@
+import java.util.Calendar;
+
+public class Programa {
+	
+	public double calculaElDoble(double numero) {
+		return 2 * numero;
+
+	}
+	
+	public String queHacerEnCasa(String dia) {
+		switch (dia) {
+		case "Lunes":
+			return "Jugar al parchis";
+		case "Martes":
+			return "Jugar a las cartas";
+		case "Jueves":
+			return "Jugar al domino";
+		case "Viernes":
+			return "Ver una pelicula";
+		case "Sabado":
+			return "Ver una serie";
+		case "Domingo":
+			return "Ver partidos repetidos";
+		default:
+			return "Este dia no existe";
+		}
+
+
+	}
+
+	public static void main(String[] args) {
+		Programa p= new Programa ();
+		System.out.println(p.calculaElDoble(2));
+	    	System.out.println(p.queHacerEnCasa("Lungsges"));
+
+	}
+
+}
